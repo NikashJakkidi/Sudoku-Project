0ecb6273eaa8a0c7a1120ae4f1f9cd46a751a8fe
NikashJakkidi <166665418+NikashJakkidi@users.noreply.github.com>
2024-04-27 18:18:12 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -78,25 +78,70 @@ elif difficulty == 'hard':
 print(removed_cells)
 board_array = generate_sudoku(9, removed_cells)
 print(board_array)
-print('Chicken')
+
 for i in range(len(board_array)):
     for j in range(len(board_array[i])):
         cell = Cell(board_array[i][j], i, j, screen)
         if board_array[i][j] != 0:
             cell.draw()
             print('cat')
+
+#reset, restart, and quit buttons(location needs to be changed)
+reset = Board.FONT.render('RESET', 0, Board.BLACK)
+reset_rect = reset.get_rect(center=(110, 650))
+pygame.draw.rect(screen, (255, 165, 0), (50, 630, 125, 40))
+screen.blit(reset, reset_rect)
+
+restart = Board.FONT.render('RESTART', 0, Board.BLACK)
+restart_rect = restart.get_rect(center=(300, 650))
+pygame.draw.rect(screen, (255, 165, 0), (475 / 2, 630, 125, 40))
+screen.blit(restart, restart_rect)
+
+quit = Board.FONT.render('QUIT', 0, Board.BLACK)
+quit_rect = third_diff.get_rect(center=(485, 650))
+pygame.draw.rect(screen, (255, 165, 0), (425, 630, 125, 40))
+screen.blit(quit, quit_rect)
+
 pygame.display.update()
 print('dog')
-pygame.time.wait(5000)
-    #for event in pygame.event.get():
-    #    if event.type == pygame.MOUSEBUTTONDOWN:
-    #        print(0)
+pygame.time.wait(10000)
 
 
-#board = Board(Board.SCREEN_WIDTH, Board.SCREEN_HEIGHT,'easy',screen)
-#while True:
- #   for event in pygame.event.get():
-  #      if event.type == pygame.QUIT:
-   #         pygame.quit()
-   #         sys.exit()
 
+
+
+#reset, restart, and quit buttons(location needs to be changed)
+    #reset = Board.FONT.render('RESET', 0, Board.BLACK)
+    #reset_rect = reset.get_rect(center=(110, 420))
+    #pygame.draw.rect(screen, (255, 165, 0), (50, 400, 125, 40))
+    #screen.blit(reset, reset_rect)
+
+    #restart = Board.FONT.render('MEDIUM', 0, Board.BLACK)
+    #restart_rect = restart.get_rect(center=(300, 420))
+    #pygame.draw.rect(screen, (255, 165, 0), (475 / 2, 400, 125, 40))
+    #screen.blit(restart, restart_rect)
+
+    #quit = Board.FONT.render('HARD', 0, Board.BLACK)
+    #quit_rect = third_diff.get_rect(center=(485, 420))
+    #pygame.draw.rect(screen, (255, 165, 0), (425, 400, 125, 40))
+    #screen.blit(quit, quit_rect)
+'''
+#Game Over Screen
+g_over = (Board.FONT).render('Game Over:(', 0, Board.BLACK)
+g_over_rect = g_over.get_rect(center=(297, 150))
+screen.blit(g_over, g_over_rect)
+rstart_diff = Board.FONT.render('Restart', 0, Board.BLACK)
+rstart_rect = rstart_diff.get_rect(center=(300, 420))
+pygame.draw.rect(screen, (255, 165, 0), (475 / 2, 400, 125, 40))
+screen.blit(rstart_diff, rstart_rect)
+
+#Game Won Screen
+g_won = (Board.FONT).render('Game Won!', 0, Board.BLACK)
+g_won_rect = g_won.get_rect(center=(297, 150))
+screen.blit(g_won, g_won_rect)
+exit_diff = Board.FONT.render('Exit', 0, Board.BLACK)
+exit_rect = exit_diff.get_rect(center=(300, 420))
+pygame.draw.rect(screen, (255, 165, 0), (475 / 2, 400, 125, 40))
+screen.blit(exit_diff, exit_rect)
+
+'''
